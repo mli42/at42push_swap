e59ac55c9fd488efcabca9aaa8d0853b5d36eaa2
mli42 <mli@student.42.fr>
2021-10-31 18:49:10 +0100

    Print stack properly

--- srcs/common/parse.c
+++ srcs/common/parse.c
@@ -6,14 +6,14 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 15:52:23 by mli               #+#    #+#             */
-/*   Updated: 2021/10/31 18:11:15 by mli              ###   ########.fr       */
+/*   Updated: 2021/10/31 18:44:25 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 #include "common_static.h"
 
-static void populate(t_stack *stack, const char **argv)
+static void	populate(t_stack *stack, const char **argv)
 {
 	int	i;
 	int	j;
@@ -21,17 +21,19 @@ static void populate(t_stack *stack, const char **argv)
 	i = -1;
 	j = 0;
 	while (argv[++i])
+	{
 		while (*argv[i])
 		{
 			if (ft_atoi_check(argv[i]) != 1)
 				break ;
 			stack->data[j++] = ft_atoi_ptr(&argv[i]);
 		}
+	}
 }
 
 t_bool	ft_parse(t_stack *stack, const char **argv)
 {
-	const int count = parse_count(argv);
+	const int	count = parse_count(argv);
 
 	if (count == -1)
 		return (false);
--- srcs/common/printStack.c
+++ srcs/common/printStack.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 16:25:20 by mli               #+#    #+#             */
-/*   Updated: 2021/10/31 17:18:33 by mli              ###   ########.fr       */
+/*   Updated: 2021/10/31 18:43:18 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 void	print_stack(t_stack stack)
 {
-	int	i;
+	int			i;
+	const char	pipe = '|';
+	const char	sep = ' ';
 
 	i = 0;
 	while (i < stack.size)
-		printf(" %d", stack.data[i++]);
+	{
+		if (stack.sep == i)
+			printf("%c", pipe);
+		else if (i != 0)
+			printf("%c", sep);
+		printf("%d", stack.data[i++]);
+	}
+	if (stack.sep == i)
+		printf("%c", pipe);
 	printf("\n");
 }
