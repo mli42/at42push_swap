a2f3ebcac49abb60fed03fe7d76670015426826a
mli42 <mli@student.42.fr>
2021-10-31 18:29:17 +0100

    Parsing: having sign w/o nb is error

--- srcs/libft/srcs/ft_atoi_checkptr.c
+++ srcs/libft/srcs/ft_atoi_checkptr.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 16:11:25 by mli               #+#    #+#             */
-/*   Updated: 2021/10/31 17:42:09 by mli              ###   ########.fr       */
+/*   Updated: 2021/10/31 18:20:11 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,26 @@
  */
 int	ft_atoi_checkptr(const char **const str)
 {
-	int has_number;
+	int	has_number;
+	int	has_sign;
 
+	has_sign = 0;
 	has_number = 0;
 	while (ft_isspace(**str))
 		(*str)++;
 	if (**str == '+' || **str == '-')
+	{
+		has_sign = 1;
 		(*str)++;
+	}
 	while (ft_isdigit(**str))
 	{
 		if (has_number == 0)
 			has_number = 1;
 		(*str)++;
 	}
-	if (**str != '\0' && !ft_isspace(**str))
+	if ((**str != '\0' && !ft_isspace(**str)) \
+		|| (has_number == 0 && has_sign == 1))
 		return (-1);
 	if (has_number == 0)
 		return (0);
