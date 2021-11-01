0947238e9cf401aed1563439760f338b1f9d4dfd
mli42 <mli@student.42.fr>
2021-11-01 18:02:02 +0100

    Check duplicate

--- srcs/common/parse.c
+++ srcs/common/parse.c
@@ -6,12 +6,31 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 15:52:23 by mli               #+#    #+#             */
-/*   Updated: 2021/11/01 11:19:12 by mli              ###   ########.fr       */
+/*   Updated: 2021/11/01 17:52:50 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 #include "common_static.h"
+
+static t_bool	check_duplicate(const t_stack *stack)
+{
+	int			i;
+	int			j;
+	const int	size = stack->size;
+
+	i = -1;
+	while (++i < size)
+	{
+		j = i;
+		while (++j < size)
+		{
+			if (stack->data[i] == stack->data[j])
+				return (false);
+		}
+	}
+	return (true);
+}
 
 static void	populate(t_stack *stack, const char **argv)
 {
@@ -44,5 +63,10 @@ t_bool	ft_parse(t_stack *stack, const char **argv)
 	stack->sep = count;
 	stack->size = count;
 	populate(stack, argv);
+	if (check_duplicate(stack) == false)
+	{
+		ft_free((void **)&stack->data);
+		return (false);
+	}
 	return (true);
 }
